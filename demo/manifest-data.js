b2b36aaa36a00f13ceb0ad286f7172f2c5985ff8
Akio Hayakawa <akio080314@gmail.com>
2026-06-29 00:17:18 +0900

    Add LF-MultiDiffusion project page

---
+++ demo/manifest-data.js
@@ -0,0 +1,364 @@
+window.EMBEDDED_MANIFEST = {
+  "methods": [
+    {
+      "id": "LFMultiDiffusion",
+      "name": "LFMultiDiffusion (Ours)"
+    },
+    {
+      "id": "DynamicScaler",
+      "name": "DynamicScaler"
+    },
+    {
+      "id": "SphereDiff",
+      "name": "SphereDiff"
+    },
+    {
+      "id": "Text2Light",
+      "name": "Text2Light"
+    },
+    {
+      "id": "PanFusion",
+      "name": "PanFusion"
+    }
+  ],
+  "prompts": [
+    {
+      "id": "underwater",
+      "text": "Underwater",
+      "items": {
+        "LFMultiDiffusion": {
+          "png": "LFMultiDiffusion/underwater.png",
+          "mp4": "LFMultiDiffusion/underwater.mp4"
+        },
+        "DynamicScaler": {
+          "png": "DynamicScaler/underwater.png",
+          "mp4": "DynamicScaler/underwater.mp4"
+        },
+        "SphereDiff": {
+          "png": "SphereDiff/underwater.png",
+          "mp4": "SphereDiff/underwater.mp4"
+        },
+        "Text2Light": {
+          "png": "Text2Light/underwater.png",
+          "mp4": "Text2Light/underwater.mp4"
+        },
+        "PanFusion": {
+          "png": "PanFusion/underwater.png",
+          "mp4": "PanFusion/underwater.mp4"
+        }
+      }
+    },
+    {
+      "id": "firefly",
+      "text": "Firefly",
+      "items": {
+        "LFMultiDiffusion": {
+          "png": "LFMultiDiffusion/firefly.png",
+          "mp4": "LFMultiDiffusion/firefly.mp4"
+        },
+        "DynamicScaler": {
+          "png": "DynamicScaler/firefly.png",
+          "mp4": "DynamicScaler/firefly.mp4"
+        },
+        "SphereDiff": {
+          "png": "SphereDiff/firefly.png",
+          "mp4": "SphereDiff/firefly.mp4"
+        },
+        "Text2Light": {
+          "png": "Text2Light/firefly.png",
+          "mp4": "Text2Light/firefly.mp4"
+        },
+        "PanFusion": {
+          "png": "PanFusion/firefly.png",
+          "mp4": "PanFusion/firefly.mp4"
+        }
+      }
+    },
+    {
+      "id": "grand_canyon",
+      "text": "Grand canyon",
+      "items": {
+        "LFMultiDiffusion": {
+          "png": "LFMultiDiffusion/grand_canyon.png",
+          "mp4": "LFMultiDiffusion/grand_canyon.mp4"
+        },
+        "DynamicScaler": {
+          "png": "DynamicScaler/grand_canyon.png",
+          "mp4": "DynamicScaler/grand_canyon.mp4"
+        },
+        "SphereDiff": {
+          "png": "SphereDiff/grand_canyon.png",
+          "mp4": "SphereDiff/grand_canyon.mp4"
+        },
+        "Text2Light": {
+          "png": "Text2Light/grand_canyon.png",
+          "mp4": "Text2Light/grand_canyon.mp4"
+        },
+        "PanFusion": {
+          "png": "PanFusion/grand_canyon.png",
+          "mp4": "PanFusion/grand_canyon.mp4"
+        }
+      }
+    },
+    {
+      "id": "firework",
+      "text": "Firework",
+      "items": {
+        "LFMultiDiffusion": {
+          "png": "LFMultiDiffusion/firework.png",
+          "mp4": "LFMultiDiffusion/firework.mp4"
+        },
+        "DynamicScaler": {
+          "png": "DynamicScaler/firework.png",
+          "mp4": "DynamicScaler/firework.mp4"
+        },
+        "SphereDiff": {
+          "png": "SphereDiff/firework.png",
+          "mp4": "SphereDiff/firework.mp4"
+        },
+        "Text2Light": {
+          "png": "Text2Light/firework.png",
+          "mp4": "Text2Light/firework.mp4"
+        },
+        "PanFusion": {
+          "png": "PanFusion/firework.png",
+          "mp4": "PanFusion/firework.mp4"
+        }
+      }
+    },
+    {
+      "id": "ocean",
+      "text": "Ocean",
+      "items": {
+        "LFMultiDiffusion": {
+          "png": "LFMultiDiffusion/ocean.png",
+          "mp4": "LFMultiDiffusion/ocean.mp4"
+        },
+        "DynamicScaler": {
+          "png": "DynamicScaler/ocean.png",
+          "mp4": "DynamicScaler/ocean.mp4"
+        },
+        "SphereDiff": {
+          "png": "SphereDiff/ocean.png",
+          "mp4": "SphereDiff/ocean.mp4"
+        },
+        "Text2Light": {
+          "png": "Text2Light/ocean.png",
+          "mp4": "Text2Light/ocean.mp4"
+        },
+        "PanFusion": {
+          "png": "PanFusion/ocean.png",
+          "mp4": "PanFusion/ocean.mp4"
+        }
+      }
+    },
+    {
+      "id": "forest",
+      "text": "Forest",
+      "items": {
+        "LFMultiDiffusion": {
+          "png": "LFMultiDiffusion/forest.png",
+          "mp4": "LFMultiDiffusion/forest.mp4"
+        },
+        "DynamicScaler": {
+          "png": "DynamicScaler/forest.png",
+          "mp4": "DynamicScaler/forest.mp4"
+        },
+        "SphereDiff": {
+          "png": "SphereDiff/forest.png",
+          "mp4": "SphereDiff/forest.mp4"
+        },
+        "Text2Light": {
+          "png": "Text2Light/forest.png",
+          "mp4": "Text2Light/forest.mp4"
+        },
+        "PanFusion": {
+          "png": "PanFusion/forest.png",
+          "mp4": "PanFusion/forest.mp4"
+        }
+      }
+    },
+    {
+      "id": "snow_mountain",
+      "text": "Snow mountain",
+      "items": {
+        "LFMultiDiffusion": {
+          "png": "LFMultiDiffusion/snow_mountain.png",
+          "mp4": "LFMultiDiffusion/snow_mountain.mp4"
+        },
+        "DynamicScaler": {
+          "png": "DynamicScaler/snow_mountain.png",
+          "mp4": "DynamicScaler/snow_mountain.mp4"
+        },
+        "SphereDiff": {
+          "png": "SphereDiff/snow_mountain.png",
+          "mp4": "SphereDiff/snow_mountain.mp4"
+        },
+        "Text2Light": {
+          "png": "Text2Light/snow_mountain.png",
+          "mp4": "Text2Light/snow_mountain.mp4"
+        },
+        "PanFusion": {
+          "png": "PanFusion/snow_mountain.png",
+          "mp4": "PanFusion/snow_mountain.mp4"
+        }
+      }
+    },
+    {
+      "id": "desert",
+      "text": "Desert",
+      "items": {
+        "LFMultiDiffusion": {
+          "png": "LFMultiDiffusion/desert.png",
+          "mp4": "LFMultiDiffusion/desert.mp4"
+        },
+        "DynamicScaler": {
+          "png": "DynamicScaler/desert.png",
+          "mp4": "DynamicScaler/desert.mp4"
+        },
+        "SphereDiff": {
+          "png": "SphereDiff/desert.png",
+          "mp4": "SphereDiff/desert.mp4"
+        },
+        "Text2Light": {
+          "png": "Text2Light/desert.png",
+          "mp4": "Text2Light/desert.mp4"
+        },
+        "PanFusion": {
+          "png": "PanFusion/desert.png",
+          "mp4": "PanFusion/desert.mp4"
+        }
+      }
+    },
+    {
+      "id": "storm",
+      "text": "Storm",
+      "items": {
+        "LFMultiDiffusion": {
+          "png": "LFMultiDiffusion/storm.png",
+          "mp4": "LFMultiDiffusion/storm.mp4"
+        },
+        "DynamicScaler": {
+          "png": "DynamicScaler/storm.png",
+          "mp4": "DynamicScaler/storm.mp4"
+        },
+        "SphereDiff": {
+          "png": "SphereDiff/storm.png",
+          "mp4": "SphereDiff/storm.mp4"
+        },
+        "Text2Light": {
+          "png": "Text2Light/storm.png",
+          "mp4": "Text2Light/storm.mp4"
+        },
+        "PanFusion": {
+          "png": "PanFusion/storm.png",
+          "mp4": "PanFusion/storm.mp4"
+        }
+      }
+    },
+    {
+      "id": "air_balloons",
+      "text": "Air balloons",
+      "items": {
+        "LFMultiDiffusion": {
+          "png": "LFMultiDiffusion/air_balloons.png",
+          "mp4": "LFMultiDiffusion/air_balloons.mp4"
+        },
+        "DynamicScaler": {
+          "png": "DynamicScaler/air_balloons.png",
+          "mp4": "DynamicScaler/air_balloons.mp4"
+        },
+        "SphereDiff": {
+          "png": "SphereDiff/air_balloons.png",
+          "mp4": "SphereDiff/air_balloons.mp4"
+        },
+        "Text2Light": {
+          "png": "Text2Light/air_balloons.png",
+          "mp4": "Text2Light/air_balloons.mp4"
+        },
+        "PanFusion": {
+          "png": "PanFusion/air_balloons.png",
+          "mp4": "PanFusion/air_balloons.mp4"
+        }
+      }
+    },
+    {
+      "id": "blossom",
+      "text": "Blossom",
+      "items": {
+        "LFMultiDiffusion": {
+          "png": "LFMultiDiffusion/blossom.png",
+          "mp4": "LFMultiDiffusion/blossom.mp4"
+        },
+        "DynamicScaler": {
+          "png": "DynamicScaler/blossom.png",
+          "mp4": "DynamicScaler/blossom.mp4"
+        },
+        "SphereDiff": {
+          "png": "SphereDiff/blossom.png",
+          "mp4": "SphereDiff/blossom.mp4"
+        },
+        "Text2Light": {
+          "png": "Text2Light/blossom.png",
+          "mp4": "Text2Light/blossom.mp4"
+        },
+        "PanFusion": {
+          "png": "PanFusion/blossom.png",
+          "mp4": "PanFusion/blossom.mp4"
+        }
+      }
+    },
+    {
+      "id": "moonlit_snowy_village",
+      "text": "Moonlit snowy village",
+      "items": {
+        "LFMultiDiffusion": {
+          "png": "LFMultiDiffusion/moonlit_snowy_village.png",
+          "mp4": "LFMultiDiffusion/moonlit_snowy_village.mp4"
+        },
+        "DynamicScaler": {
+          "png": "DynamicScaler/moonlit_snowy_village.png",
+          "mp4": "DynamicScaler/moonlit_snowy_village.mp4"
+        },
+        "SphereDiff": {
+          "png": "SphereDiff/moonlit_snowy_village.png",
+          "mp4": "SphereDiff/moonlit_snowy_village.mp4"
+        },
+        "Text2Light": {
+          "png": "Text2Light/moonlit_snowy_village.png",
+          "mp4": "Text2Light/moonlit_snowy_village.mp4"
+        },
+        "PanFusion": {
+          "png": "PanFusion/moonlit_snowy_village.png",
+          "mp4": "PanFusion/moonlit_snowy_village.mp4"
+        }
+      }
+    },
+    {
+      "id": "rock_mountain",
+      "text": "Rock mountain",
+      "items": {
+        "LFMultiDiffusion": {
+          "png": "LFMultiDiffusion/rock_mountain.png",
+          "mp4": "LFMultiDiffusion/rock_mountain.mp4"
+        },
+        "DynamicScaler": {
+          "png": "DynamicScaler/rock_mountain.png",
+          "mp4": "DynamicScaler/rock_mountain.mp4"
+        },
+        "SphereDiff": {
+          "png": "SphereDiff/rock_mountain.png",
+          "mp4": "SphereDiff/rock_mountain.mp4"
+        },
+        "Text2Light": {
+          "png": "Text2Light/rock_mountain.png",
+          "mp4": "Text2Light/rock_mountain.mp4"
+        },
+        "PanFusion": {
+          "png": "PanFusion/rock_mountain.png",
+          "mp4": "PanFusion/rock_mountain.mp4"
+        }
+      }
+    }
+  ]
+};
